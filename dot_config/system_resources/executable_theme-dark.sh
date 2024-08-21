ea2b63ffb42233ee50f6d9599dbdb3ef25587959
Toms Jansons <toms.jansons@hood-software.lv>
2024-08-22 02:39:22 +0300

    dark and light theme switcher

--- dot_config/system_resources/executable_theme-dark.sh
+++ dot_config/system_resources/executable_theme-dark.sh
@@ -2,6 +2,11 @@
 
 notify-send --app-name="theme-switcher" --urgency=low --icon=weather-clear-night "switching to dark mode"
 
-for server in $(nvr --serverlist); do
-    nvr --servername "$server" -cc 'set background=dark'
+ls "/run/user/1000/" | grep 'nvim' | while read socket; do
+	nvim --server "/run/user/1000/$socket" --remote-send "<esc>:set background=dark<cr>"
 done
+
+ls "/run/user/1000/" | grep 'Alacritty' | while read socket; do
+	alacritty msg -s "/run/user/1000/$socket" config "$(cat ~/.config/alacritty/gruvbox_dark.toml)" -w -1
+done
+
--- dot_config/system_resources/executable_theme-light.sh
+++ dot_config/system_resources/executable_theme-light.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-notify-send --app-name="darkman" --urgency=low --icon=weather-clear "switching to light mode"
+notify-send --app-name="theme-switcher" --urgency=low --icon=weather-clear "switching to light mode"
 
+ls "/run/user/1000/" | grep 'nvim' | while read socket; do
+	nvim --server "/run/user/1000/$socket" --remote-send "<esc>:set background=light<cr>"
+done
 
-for server in $(nvr --serverlist); do
-    nvr --servername "$server" -cc 'set background=light'
+ls "/run/user/1000/" | grep 'Alacritty' | while read socket; do
+	alacritty msg -s "/run/user/1000/$socket" config "$(cat ~/.config/alacritty/gruvbox_light.toml)" -w -1
 done
