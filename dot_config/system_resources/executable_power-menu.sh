930223255e78aef4727c7682e06cd6737ce80e4c
Toms Jansons <toms.jansons@hood-software.lv>
2024-08-26 01:28:44 +0300

    wofi fix

--- dot_config/system_resources/executable_power-menu.sh
+++ dot_config/system_resources/executable_power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n󱉚 Hibernate\n Suspend\n Lock\n Logout" | bash /home/toms/.config/system_resources/wofi.sh | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n󱉚 Hibernate\n Suspend\n Lock\n Logout" | bash /home/toms/.config/system_resources/wofi.sh -S dmenu | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
