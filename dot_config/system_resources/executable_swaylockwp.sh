b897f44f7ee3d2d94e6d58893fe1d52da0e1f26a
Toms Jansons <toms.jansons@hood-software.lv>
2024-08-31 01:10:09 +0300

    bw agent

--- dot_config/system_resources/executable_swaylockwp.sh
+++ dot_config/system_resources/executable_swaylockwp.sh
@@ -1,5 +1,7 @@
-if [ -e ~/.theme-light ]; then
-	swaylock -i /home/toms/.config/system_resources/astronaut_wp_lighter.jpg
-else
-	swaylock -i /home/toms/.config/system_resources/astronaut_wp.jpg
+if ! pgrep -x 'swaylock' > /dev/null; then
+	if [ -e ~/.theme-light ]; then
+		swaylock -i /home/toms/.config/system_resources/astronaut_wp_lighter.jpg
+	else
+		swaylock -i /home/toms/.config/system_resources/astronaut_wp.jpg
+	fi
 fi
