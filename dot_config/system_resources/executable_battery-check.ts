644e26b18a88b7fcc27b8822fb9ddcbde4947f78
Toms Jansons <toms.jansons@hood-software.lv>
2024-12-02 16:47:21 +0200

    monitors#

--- dot_config/system_resources/executable_battery-check.ts
+++ dot_config/system_resources/executable_battery-check.ts
@@ -16,6 +16,7 @@ async function getPluggedState() {
 }
 
 async function getBatteryLevel() {
+	await $`acpi`;
   const output: string = await $`acpi`.text();
   const percentStr = output.split(" ")[3].split("%")[0];
   const percent = Number(percentStr);
@@ -32,7 +33,32 @@ while (true) {
     if (await markerFile.exists()) {
       await $`rm /tmp/battery-check-marker`;
     }
-
+//
+		//Nov 24 00:21:26 arch-l13 systemd[1]: battery-check.service: Main process exited, code=exited, status=1/FAILURE
+// Nov 24 00:21:26 arch-l13 systemd[1]: battery-check.service: Failed with result 'exit-code'.
+// Nov 24 00:21:29 arch-l13 systemd[1]: battery-check.service: Scheduled restart job, restart counter is at 23.
+// Nov 24 00:21:29 arch-l13 systemd[1]: Started Battery check service.
+// Nov 24 00:21:29 arch-l13 bun[8735]: Battery 0: Discharging, 9%, 00:24:50 remaining
+// Nov 24 00:21:29 arch-l13 bun[8735]: Cannot autolaunch D-Bus without X11 $DISPLAY
+// Nov 24 00:21:29 arch-l13 bun[8735]: 36 |
+// Nov 24 00:21:29 arch-l13 bun[8735]: 37 |     if (batPercent < CRIT_BAT) {
+// Nov 24 00:21:29 arch-l13 bun[8735]: 38 |       await $`notify-send -u critical -i /home/toms/.config/system_resources/icons8-sleep-50.png "Battery critical - hibernating"`;
+// Nov 24 00:21:29 arch-l13 bun[8735]: 39 |       await $`systemctl hibernate`;
+// Nov 24 00:21:29 arch-l13 bun[8735]: 40 |     } else if (batPercent < MIN_BAT) {
+// Nov 24 00:21:29 arch-l13 bun[8735]: 41 |       await $`notify-send -u critical -i /home/toms/.config/system_resources/icons8-android-l-battery-64.png "Battery below ${MIN_BAT}"`;
+// Nov 24 00:21:29 arch-l13 bun[8735]:                     ^
+// Nov 24 00:21:29 arch-l13 bun[8735]: ShellError: Failed with exit code 1
+// Nov 24 00:21:29 arch-l13 bun[8735]:  info: {
+// Nov 24 00:21:29 arch-l13 bun[8735]:   "exitCode": 1,
+// Nov 24 00:21:29 arch-l13 bun[8735]:   "stderr": "Cannot autolaunch D-Bus without X11 $DISPLAY\n",
+// Nov 24 00:21:29 arch-l13 bun[8735]:   "stdout": ""
+// Nov 24 00:21:29 arch-l13 bun[8735]: }
+// Nov 24 00:21:29 arch-l13 bun[8735]:       at new ShellError (12:16)
+// Nov 24 00:21:29 arch-l13 bun[8735]:       at new ShellPromise (72:16)
+// Nov 24 00:21:29 arch-l13 bun[8735]:       at BunShell (187:35)
+// Nov 24 00:21:29 arch-l13 bun[8735]:       at /home/toms/.config/system_resources/battery-check.ts:41:13
+// Nov 24 00:21:29 arch-l13 bun[8735]: Bun v1.1.34 (Linux x64)
+// Nov 24 00:21:29 arch-l13 systemd[1]: battery-check.service: Main process exited, code=exited, status=1/FAILURE
     if (batPercent < CRIT_BAT) {
       await $`notify-send -u critical -i /home/toms/.config/system_resources/icons8-sleep-50.png "Battery critical - hibernating"`;
       await $`systemctl hibernate`;
