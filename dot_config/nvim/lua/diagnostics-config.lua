9ba2d273ff7edb4aea482b850e071d798f33ebe3
Toms Jansons <toms.jansons@hood-software.lv>
2025-01-30 01:02:54 +0200

    removing old nvim stuff

--- dot_config/nvim/lua/diagnostics-config.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = '[D]iagnostics [L]ist' })
-
-vim.keymap.set('n', '<Leader>df', vim.diagnostic.open_float,
-  { noremap = true, silent = true, desc = '[D]iagnostics [F]loat window' })
-vim.keymap.set('n', '<Leader>dn', vim.diagnostic.goto_next,
-  { noremap = true, silent = true, desc = '[D]iagnostics [N]ext' })
-vim.keymap.set('n', '<Leader>dp', vim.diagnostic.goto_prev,
-  { noremap = true, silent = true, desc = '[D]iagnostics [P]rev' })
