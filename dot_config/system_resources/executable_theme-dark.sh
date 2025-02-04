8571d655311d666b60f0272b924e33c7571d8cc2
Toms Jansons <toms.jansons@hood-software.lv>
2025-02-04 11:26:42 +0200

    lazyvim changes

--- dot_config/system_resources/executable_theme-dark.sh
+++ dot_config/system_resources/executable_theme-dark.sh
@@ -5,12 +5,13 @@ touch ~/.theme-dark
 
 notify-send --app-name="theme-switcher" --urgency=low --icon=weather-clear-night "switching to dark mode"
 
+sed -i 's/catppuccin-latte/catppuccin-mocha/g' /home/toms/.config/nvim/lua/plugins/colorscheme.lua
 ls "/run/user/1000/" | grep 'nvim' | while read socket; do
-nvim --server "/run/user/1000/$socket" --remote-send "<esc>:colorscheme catppuccin-mocha<cr><esc>:lua require('lualine').setup({options={theme='codedark'}})<cr><esc>:lua require('local-highlight').setup()<cr>"
+  nvim --server "/run/user/1000/$socket" --remote-send "<esc>:colorscheme catppuccin-mocha<cr>"
 done
 
 ls "/run/user/1000/" | grep 'Alacritty' | while read socket; do
-	alacritty msg -s "/run/user/1000/$socket" config "$(cat ~/.config/alacritty/catppuccin-mocha.toml)" -w -1
+  alacritty msg -s "/run/user/1000/$socket" config "$(cat ~/.config/alacritty/catppuccin-mocha.toml)" -w -1
 done
 
 killall swaybg && /home/toms/.config/system_resources/swaybg.sh &
@@ -22,16 +23,16 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 sed -i 's/7287fd/cdd6f4/g' /home/toms/.config/niri/config.kdl
 sed -i 's/dc8a78/f5e0dc/g' /home/toms/.config/niri/config.kdl
 
-echo '@import url("mocha.css");' > /home/toms/.config/swaync/style.css
+echo '@import url("mocha.css");' >/home/toms/.config/swaync/style.css
 swaync-client -rs
 
 sed -i 's/latte.css/mocha.css/g' /home/toms/.config/waybar/style.css
 killall waybar
-waybar & disown
+waybar &
+disown
 
 sed -i 's/ansi/TwoDark/g' /home/toms/.config/bat/config
 
 sed -i 's/latte/mocha/g' /home/toms/.config/ghostty/config
 
 sed -i 's/catppuccin_latte/catppuccin_mocha/g' /home/toms/.config/helix/config.toml
-
