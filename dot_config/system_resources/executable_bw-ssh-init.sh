0d11b9e5a85bd65f1224bd905ee1f54028dfe178
Toms Jansons <toms.jansons@hood-software.lv>
2025-03-07 17:11:03 +0200

    switch to zsh

--- dot_config/system_resources/executable_bw-ssh-init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-export BW_SESSION=$(bw unlock --raw)
-
-eval $(ssh-agent -s -t 1h)
-
-bw_add_sshkeys
-
-zsh
--- executable_chezmoi-apply.nu
+++
@@ -1,4 +0,0 @@
-if 'BW_SESSION' not-in $env {
-	{BW_SESSION:(bw unlock --raw)} | load-env
-}
-chezmoi apply
