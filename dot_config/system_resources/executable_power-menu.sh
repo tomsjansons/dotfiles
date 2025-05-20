284d8cb40feca1eeb169036455ec5295998702ee
Toms Jansons <toms.jansons@hood-software.lv>
2025-05-20 11:41:07 +0300

    power menu

--- dot_config/system_resources/executable_power-menu.sh
+++ dot_config/system_resources/executable_power-menu.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n󱉚 Hibernate\n Suspend\n Lock\n Logout" | bash /home/toms/.config/system_resources/wofi.sh -S dmenu | awk '{print tolower($2)}' )
+op=$(echo -e " poweroff\n reboot\n󱉚 hibernate\n suspend\n lock\n logout" | rofi -dmenu | awk '{print tolower($2)}')
 
-case $op in 
-        poweroff)
-                ;&
-        reboot)
-                systemctl $op
-                ;;
-        hibernate)
-                # hyprlock & sleep 3 && systemctl $op
-                systemctl $op
-                ;;
-        suspend)
-                # hyprlock & sleep 3 && systemctl $op
-                systemctl $op
-                ;;
-        lock)
-                # hyprlock
-                exec /home/toms/.config/system_resources/swaylockwp.sh
-                ;;
-        logout)
-                # hyprctl dispatch exit
-                niri msg action quit
-                ;;
+case $op in
+poweroff) ;&
+reboot)
+  systemctl $op
+  ;;
+hibernate)
+  # hyprlock & sleep 3 && systemctl $op
+  systemctl $op
+  ;;
+suspend)
+  # hyprlock & sleep 3 && systemctl $op
+  systemctl $op
+  ;;
+lock)
+  # hyprlock
+  exec /home/toms/.config/system_resources/swaylockwp.sh
+  ;;
+logout)
+  # hyprctl dispatch exit
+  niri msg action quit
+  ;;
 esac
