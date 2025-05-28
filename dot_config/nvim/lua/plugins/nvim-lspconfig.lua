4a88e4ed92e0c770b1fc8da3dc51af66b1725f24
Toms Jansons <toms.jansons@hood-software.lv>
2025-05-29 01:08:15 +0300

    package cleanup

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,21 @@
 local nvim_lsp = require("lspconfig")
 
+vim.filetype.add({
+  pattern = {
+    [".*/.jinja-*"] = "jinja",
+  },
+})
+
 return {
   {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
       servers = {
+        jinja_lsp = {
+          filetypes = { "jinja", "html" },
+        },
+        eslint = {},
         denols = {
           filetypes = { "typescript", "typescriptreact" },
           root_dir = function(...)
@@ -18,6 +28,17 @@ return {
         biome = {
           root_dir = nvim_lsp.util.root_pattern("biome.json", "biome.jsonc"),
         },
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
       },
     },
   },
