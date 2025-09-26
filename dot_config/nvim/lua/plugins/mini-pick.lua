a825e6b62f64462c1b19f193e9ebb3cce0ca4fd9
Toms Jansons <toms.jansons@hood-software.lv>
2025-09-27 00:35:12 +0300

    mini-pick old

--- dot_config/nvim/lua/plugins/mini-pick.lua
+++ dot_config/nvim/lua/plugins/mini-pick.lua
@@ -9,19 +9,47 @@ local pick = require("mini.pick")
 local extra = require("mini.extra")
 vim.ui.select = pick.ui_select
 
+-- vim.schedule callback: /home/toms/.config/nvim/lua/keymaps.lua:28: Vim:E325: ATTENTION
+-- stack traceback:
+-- 	[C]: in function 'nvim_win_set_buf'
+-- 	/home/toms/.config/nvim/lua/keymaps.lua:28: in function 'StepBackJumplist'
+-- 	/home/toms/.config/nvim/lua/plugins/mini-pick.lua:15: in function 'func'
+-- 	...hare/nvim/site/pack/core/opt/mini.pick/lua/mini/pick.lua:2206: in function 'start'
+-- 	...hare/nvim/site/pack/core/opt/mini.pick/lua/mini/pick.lua:898: in function ''
+-- 	vim/_editor.lua: in function ''
+-- 	vim/_editor.lua: in function <vim/_editor.lua:0>
+-- Client stylua quit with exit code 2 and signal 0. Check log for errors: /home/toms/.local/state/nvim/lsp.log
 local function open_buffers()
-	local wipeout_cur = function()
-		vim.api.nvim_buf_delete(MiniPick.get_picker_matches().current.bufnr, {})
+	local delete_selected_buf = function()
+		local buf = MiniPick.get_picker_matches().current.bufnr
+		StepBackJumplist(buf)
+		vim.api.nvim_buf_delete(buf, { force = true })
 		MiniPick.stop()
 		open_buffers()
 	end
-	local buffer_mappings = { wipeout = { char = "<C-d>", func = wipeout_cur } }
+	local buffer_mappings = { wipeout = { char = "<C-d>", func = delete_selected_buf } }
 	MiniPick.builtin.buffers({}, { mappings = buffer_mappings })
 end
 
 vim.keymap.set("n", "<leader>b", open_buffers, { desc = "Buffers" })
 
-vim.keymap.set("n", "gr", function()
+vim.keymap.set("n", "gri", function()
+	extra.pickers.lsp({ scope = "implementation" })
+end, { desc = "Pick implementation" })
+
+vim.keymap.set("n", "grn", function()
+	extra.pickers.lsp({ scope = "references" })
+end, { desc = "Pick references" })
+
+vim.keymap.set("n", "grr", function()
+	extra.pickers.lsp({ scope = "references" })
+end, { desc = "Pick references" })
+
+vim.keymap.set("n", "grt", function()
+	extra.pickers.lsp({ scope = "references" })
+end, { desc = "Pick references" })
+
+vim.keymap.set("n", "grO", function()
 	extra.pickers.lsp({ scope = "references" })
 end, { desc = "Pick references" })
 
