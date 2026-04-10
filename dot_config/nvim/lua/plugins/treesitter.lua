66e1684cf01ef0ab9f128740e05f464edcda5f62
Toms Jansons <toms.jansons@hood-software.lv>
2026-04-10 20:11:12 +0300

    chore: nvim treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,75 +4,65 @@ vim.pack.add({
 	{ src = "https://github.com/mawkler/jsx-element.nvim" },
 })
 
+local parsers = {
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"typescript",
+	"tsx",
+	"go",
+	"rust",
+	"zig",
+	"yaml",
+	"kotlin",
+	"swift",
+}
+
 require("nvim-treesitter").setup()
 
-require("nvim-treesitter.configs").setup({
-	modules = {},
-	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = {
-		"lua",
+local installed = require("nvim-treesitter").get_installed()
+local missing = vim.tbl_filter(function(lang)
+	return not vim.list_contains(installed, lang)
+end, parsers)
+
+if #missing > 0 then
+	require("nvim-treesitter").install(missing, { summary = true })
+end
+
+require("nvim-treesitter-textobjects").setup({
+	select = {
+		lookahead = true,
+		keymaps = {
+			["af"] = "@function.outer",
+			["if"] = "@function.inner",
+		},
+		include_surrounding_whitespace = true,
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = {
+ 		"lua",
 		"markdown",
-		"markdown_inline",
 		"typescript",
-		"tsx",
+		"typescriptreact",
 		"javascript",
+		"javascriptreact",
 		"go",
-		"rust",
-		"zig",
 		"yaml",
 		"kotlin",
 		"swift",
+		"zig",
 	},
+	callback = function(args)
+		local max_filesize = 100 * 1024
+		local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+		if ok and stats and stats.size > max_filesize then
+			return
+		end
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
-
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
-
-	-- List of parsers to ignore installing (or "all")
-	ignore_install = { "javascript" },
-
-	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-	highlight = {
-		enable = true,
-
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		disable = { "c", "rust" },
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		disable = function(lang, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = true,
-
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-			},
-			include_surrounding_whitespace = true,
-		},
-	},
+		pcall(vim.treesitter.start, args.buf)
+	end,
 })
 
 require("jsx-element").setup({
